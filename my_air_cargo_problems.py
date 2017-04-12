253b9e218a2e289b7573d91d728a03edbb98132c
James Requa <james@carteblanche.com>
2017-04-11 22:21:38 -0700

    Unload Action
    Create all Unload ground actions from the domain Unload action

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -80,6 +80,17 @@ class AirCargoProblem(Problem):
             '''
             unloads = []
             # TODO create all Unload ground actions from the domain Unload action
+            for c in self.cargos:
+                for p in self.planes:
+                    for a in self.airports:
+                        precond_pos = [expr("In({}, {})".format(c, p)), expr("At({}, {})".format(p, a))]
+                        precond_neg = []
+                        effect_add = [expr("At({}, {})".format(c, a))]
+                        effect_rem = [expr("In({}, {})".format(c, p))]
+                        unload = Action(expr("Unload({}, {}, {})".format(c, p, a)),
+                                        [precond_pos, precond_neg],
+                                        [effect_add, effect_rem])
+                        unloads.append(unload)
             return unloads
 
         def fly_actions():
