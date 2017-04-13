d803b5ce723f0a99a858084a392bdedc1a270c00
James Requa <james@carteblanche.com>
2017-04-12 23:09:39 -0700

    AirCargoProblem.result method
    Implemented the result method.
    Return the state that results from executing the given action in the
    given state.

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -150,6 +150,19 @@ class AirCargoProblem(Problem):
         """
         # TODO implement
         new_state = FluentState([], [])
+        old_state = decode_state(state, self.state_map)
+        for fluent in old_state.pos:
+            if fluent not in action.effect_rem:
+                new_state.pos.append(fluent)
+        for fluent in action.effect_add:
+            if fluent not in new_state.pos:
+                new_state.pos.append(fluent)
+        for fluent in old_state.neg:
+            if fluent not in action.effect_add:
+                new_state.neg.append(fluent)
+        for fluent in action.effect_rem:
+            if fluent not in new_state.neg:
+                new_state.neg.append(fluent)
         return encode_state(new_state, self.state_map)
 
     def goal_test(self, state: str) -> bool:
