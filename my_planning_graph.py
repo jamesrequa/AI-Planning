c5aaa2d6e06d769bf07eaaaf82e3a9716ca1d184
James Requa <james@carteblanche.com>
2017-04-16 23:30:49 -0700

    PlanningGraph.inconsistent_support_mutex
    - Implemented the inconsistent_support_mutex method for PlanningGraph
    class.
    - This method tests a pair of state literals for mutual exclusion,
    returning True if there are no actions that could achieve the two
    literals at the same time, and False otherwise.

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -522,7 +522,11 @@ class PlanningGraph():
         :return: bool
         '''
         # TODO test for Inconsistent Support between nodes
-        return False
+        for s1 in node_s1.parents:
+            for s2 in node_s2.parents:
+                if not s1.is_mutex(s2):
+                    return False
+        return True
 
     def h_levelsum(self) -> int:
         '''The sum of the level costs of the individual goals (admissible if goals independent)
